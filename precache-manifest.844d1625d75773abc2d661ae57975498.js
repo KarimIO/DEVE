aa8b1c64f72f0861d3e7c1edce4bf7bdbf997f68
Karim Abdel Hamid <karimah@aucegypt.edu>
2018-11-18 13:00:49 +0200

    Updates

---
+++ precache-manifest.844d1625d75773abc2d661ae57975498.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "52c9b2aa4d9fc8c53b64",
+    "url": "/DEVE/static/js/runtime~main.52c9b2aa.js"
+  },
+  {
+    "revision": "f6d8d8150c86cb05c888",
+    "url": "/DEVE/static/js/main.f6d8d815.chunk.js"
+  },
+  {
+    "revision": "0e2415aa9eb5761fffb2",
+    "url": "/DEVE/static/js/1.0e2415aa.chunk.js"
+  },
+  {
+    "revision": "f6d8d8150c86cb05c888",
+    "url": "/DEVE/static/css/main.b7ff463f.chunk.css"
+  },
+  {
+    "revision": "96677e3bd10e17013da31c0aa3167a1a",
+    "url": "/DEVE/index.html"
+  }
+];
